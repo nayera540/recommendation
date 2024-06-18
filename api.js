f6970d51905feeb344fa1077580d48616ae583b5
Nayera Hazem <1900205@eng.asu.edu.eg>
2024-06-18 19:54:36 +0300

    Create api.js

---
+++ api.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+const cron = require('node-cron');
+
+// Function to execute the API call
+async function executeAPI() {
+    try {
+        const response = await axios.get('https://nayera26-testapi.hf.space/run-notebook'); 
+        console.log(response.data);
+    } catch (error) {
+        console.error('Error executing API:', error.message);
+    }
+}
+
+// Schedule the API call to execute every day at midnight
+cron.schedule('0 0 * * *', () => {
+    console.log('Executing API call...');
+    executeAPI();
+});
